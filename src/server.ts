54b9590034b0aea83b567e39c6285dd9be340f78
Rosie Schofield <rschofield00@gmail.com>
2023-08-18 08:47:17 +0100

    tried to fix env var issues

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 import {
   //addDummyDbItems,
   //addDbItem,
@@ -21,9 +21,9 @@ app.use(express.json());
 /** To allow 'Cross-Origin Resource Sharing': https://en.wikipedia.org/wiki/Cross-origin_resource_sharing */
 app.use(cors());
 // read in contents of any environment variables in the .env file
-dotenv.config();
+//dotenv.config();
 // use the environment variable PORT, or 4000 as a fallback
-//const PORT_NUMBER = process.env.PORT ?? 4000;
+const PORT_NUMBER = process.env.PORT ?? 4000;
 
 // API info page
 /*app.get("/", (req, res) => {
@@ -123,7 +123,7 @@ app.put<{ id: string }, {}, Partial<DbItem>>("/todos/:id", async (req, res) => {
   await client.end();
 });
 
-/*app.listen(PORT_NUMBER, () => {
+app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
 });
-*/
+
