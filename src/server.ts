b87f390ff517c9ce5f71a2b5863fa1869ef84ef8
Rosie Schofield <rschofield00@gmail.com>
2023-08-18 08:50:34 +0100

    forgot prettier, eslint and typecheck

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-//import dotenv from "dotenv";
+import dotenv from "dotenv";
 import {
   //addDummyDbItems,
   //addDbItem,
@@ -21,7 +21,7 @@ app.use(express.json());
 /** To allow 'Cross-Origin Resource Sharing': https://en.wikipedia.org/wiki/Cross-origin_resource_sharing */
 app.use(cors());
 // read in contents of any environment variables in the .env file
-//dotenv.config();
+dotenv.config();
 // use the environment variable PORT, or 4000 as a fallback
 const PORT_NUMBER = process.env.PORT ?? 4000;
 
@@ -126,4 +126,3 @@ app.put<{ id: string }, {}, Partial<DbItem>>("/todos/:id", async (req, res) => {
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
 });
-
